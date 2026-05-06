8d8e5975a56951745437d9018206727d4ef3e653
Caleb <ayay459547@gmail.com>
2026-05-06 09:55:24 +0800

    test: nojekyll

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
   reactStrictMode: true,
-  assetPrefix: isProd
-    ? "https://ayay459547.github.io/AWS-CCP-Exam/"
-    : undefined,
+  assetPrefix: isProd ? "/AWS-CCP-Exam/" : undefined,
 };
 
 export default nextConfig;

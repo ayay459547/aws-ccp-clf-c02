52f8e0becfc7845c1944e8ef10d38af924c598b2
Caleb <ayay459547@gmail.com>
2026-05-13 13:19:09 +0800

    feat: change url

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
   reactStrictMode: true,
-  assetPrefix: isProd ? "/aws-ccp-clf-c02/" : undefined,
+  // 移除或註解掉 assetPrefix，因為現在使用獨立子網域了
+  // assetPrefix: isProd ? "/aws-ccp-clf-c02/" : undefined,
+
+  // 如果你有用到 images 或是其他路徑，通常也不需要 basePath 了
+  // basePath: isProd ? "/aws-ccp-clf-c02" : undefined,
 };
 
 export default nextConfig;
